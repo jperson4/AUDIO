cf1c16b86b6b6541d4157d0c6ec5075c91612821
Javier <73282806+jperson4@users.noreply.github.com>
2026-02-09 12:44:53 +0100

    Para que no haga falta importar todo

---
+++ libreria/pysonance/__init__.py
@@ -0,0 +1,8 @@
+from .const import *
+from .control import *
+from .filter import *
+from .IO import *
+# from .pactl import * # PRIVADO
+from .sampler import *
+from .signal import *
+from .sound import *
